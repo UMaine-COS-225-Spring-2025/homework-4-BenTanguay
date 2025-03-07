7614069906d6ed3daed6ae9bd3293d8cb84f3661
bpt06 <bentanguay06@gmail.com>
2025-03-07 11:07:27 -0500

    fixed Book.java
    fixed constructor

--- Book.java
+++ Book.java
@@ -2,7 +2,7 @@ public class Book{
     public String title;
     public String genre;
 
-    public void Book(String title, String genre){
+    public Book(String title, String genre){
         this.title = title;
         this.genre = genre;
     }
